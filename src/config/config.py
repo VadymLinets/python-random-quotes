c98ea1ad3c909f21dc7befd8f282b1c31b971f90
Vadym Linets <vadym.linets@computools.com>
2024-07-07 12:54:26 +0300

    Config improvements

--- src/config/config.py
+++ src/config/config.py
@@ -2,29 +2,21 @@ from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class ServerConfig(BaseSettings):
-    server: str = Field(alias="SERVER", default="fastapi")
-
+class Config(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         extra="ignore",
     )
 
 
-class PostgresConfig(BaseSettings):
+class ServerConfig(Config):
+    server: str = Field(alias="SERVER", default="fastapi")
+
+
+class PostgresConfig(Config):
     orm: str = Field(alias="POSTGRES_ORM", default="sqlalchemy")
     dsn: str = Field(alias="POSTGRES_DSN")
 
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        extra="ignore",
-    )
 
-
-class QuotesConfig(BaseSettings):
+class QuotesConfig(Config):
     random_quote_chance: float = Field(alias="RANDOM_QUOTE_CHANCE", default=20.0)
-
-    model_config = SettingsConfigDict(
-        env_file=".env",
-        extra="ignore",
-    )
